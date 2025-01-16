9ace4ff05bd5847ffdee32b68eef644780a15a30
Xander Bailey <xander.bailey@gmail.com>
2025-01-16 15:25:26 -0800

    Complete assignment2-2

--- scripts/tests.dart
+++ scripts/tests.dart
@@ -4,9 +4,19 @@ Future<void> main() async {
   // testing with Bend, OR coordinates
   double lat = 44.05;
   double lon = -121.31;
-  // TODO: Seach for latitutes and longitudes of 5 cities in the US on the internet
-  // Create a for loop that will generate forecasts arrays for each city
-  // TODO: create forecasts and forecastsHourly both of type List<forecast.Forecast>
-  forecast.getForecastFromPoints(lat, lon);
-  forecast.getForecastHourlyFromPoints(lat,lon);
+  List<List<double>> latLongs = [
+    [39.29, -76.61],  // Baltimore, MD
+    [21.30, -157.85], // Honolulu, HI
+    [32.71, -117.16], // San Diego, CA
+    [44.97, -93.26],  // Minneapolis, MN
+    [61.21, -149.89], // Anchorage, AK
+  ];
+
+  for (List<double> city in latLongs) {
+    lat = city[0];
+    lon = city[1];
+
+    List<forecast.Forecast> forecasts = await forecast.getForecastFromPoints(lat, lon);
+    List<forecast.Forecast> forecastsHourly = await forecast.getForecastHourlyFromPoints(lat,lon);
+  }
 }
